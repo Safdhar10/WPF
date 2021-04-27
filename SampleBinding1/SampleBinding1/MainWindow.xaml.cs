3149d60b8f1dd48a8e65685cbc4630958fbcb7eb
Safdhar R <68153873+Safdhar10@users.noreply.github.com>
2021-04-27 15:07:26 +0530

    Add files via upload
    Sample Binding

---
+++ SampleBinding1/SampleBinding1/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SampleBinding1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<model.Employee> Employees = new List<model.Employee>();
+        int currentIndex = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitEmployee();
+            MainGrid.DataContext = Employees[currentIndex];
+        }
+        private void InitEmployee()
+        {
+            Employees.Add(new model.Employee("Ashraf", 21, "9600630212", "7796 7455 2221", "CEO", "images/ashraf.jpg"));
+            Employees.Add(new model.Employee("Safdhar", 21, "9600630212", "7796 7455 2221","Manager", "images/safdhar.jpeg"));
+        }
+
+        private void Previous_Click(object sender, RoutedEventArgs e)
+        {
+            if(currentIndex>0)
+            {
+                MainGrid.DataContext = Employees[--currentIndex];
+            }
+        }
+
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+            if(currentIndex<Employees.Count-1)
+            {
+                MainGrid.DataContext = Employees[++currentIndex];
+            }
+        }
+    }
+}
---
+++ SampleBinding1/SampleBinding1/model/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleBinding1.model
+{
+    class Employee
+    {
+         public string Name { get; set; }
+        public int Age { get; set; }
+        public string Mobile { get; set; }
+        public string AadharNumber { get; set; }
+        public string Designation { get; set; }
+        public string Image { get; set; }
+        public Employee()
+        {
+
+        }
+        public Employee(string Name,int Age,string Mobile,string AadharNumber,string Designation,string Image)
+        {
+            this.Name = Name;
+            this.Age = Age;
+            this.Mobile = Mobile;
+            this.AadharNumber = AadharNumber;
+            this.Designation = Designation;
+            this.Image = Image;
+        }
+    }
+}
